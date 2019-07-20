c18cdcc147536727f2f903ec8d32a67bdb29e9ae
Karly Mareka <karly.mareka@gmail.com>
2019-07-20 15:32:21 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,8 +2,7 @@ require 'pry'
 class Helpers
   
   def self.current_user(user)
-    @id = user[:user_id]
-     @user = User.find_by(id: user[:user_id])
+    @user = User.find_by(id: user[:user_id])
   end 
 
   def self.is_logged_in?(user)
